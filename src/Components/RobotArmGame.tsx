8030a5ed9726ffe66ded3de708a9c2b19f5e429b
SuperScary <wylanshoemaker@gmail.com>
2026-03-20 23:41:50 -0700

    Remove About, App components, and related styles

--- src/Components/RobotArmGame.tsx
+++
@@ -1,7 +0,0 @@
-import Spline from '@splinetool/react-spline';
-
-export function RobotArmGame() {
-  return (
-    <Spline scene="https://prod.spline.design/Zucqx6pHtD14FShA/scene.splinecode" />
-  );
-}
